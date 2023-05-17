c8ea4e399dc489a4e1557bc5c5b833ce4d4f3047
Melissa Evangelista <melissaevangelista52@gmail.com>
2023-05-16 21:40:09 -0400

    added 'click' event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Your code goes here!
+
+///"click" event start///
+function change(e) {
+	let el = e.target;
+	if (el.style.border === "3px solid red") {
+		el.style.border = "none";
+	} else {
+		el.style.border = "3px solid red";
+	}
+}
+
+window.addEventListener("click", change);
+
+window.onclick = function (e) {
+	console.log(e.target);
+};
+///"click" event end///
+
+///"mouseover" event start///
+function imgHover(e) {
+	let el = e.target;
+	if (el.style.border === "5px solid blue") {
+		el.style.border = "none";
+	} else {
+		el.style.border = "5px solid blue";
+	}
+}
+
+const imgs = document.getElementsByClassName("img-content");
+
+///"mouseover" event end///
